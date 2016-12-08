87152dd5c53be8ab56d99257b34659c4683d9445
Android <mohitlivetech2016@gmail.com>
2016-12-08 17:32:39 +0530

    8 dec final

---
+++ app/src/main/java/com/app/marshmallowpermission/TryPermissionCheck/MainActivity.java
@@ -0,0 +1,126 @@
+package com.app.marshmallowpermission.TryPermissionCheck;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.app.marshmallowpermission.R;
+
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+
+    /**
+     * Id to identify a camera permission request.
+     */
+    private static final int REQUEST_CAMERA = 0;
+
+    public static final String TAG = "MainActivity";
+    private ImageView iv_camera, iv_pic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        iv_pic = (ImageView) findViewById(R.id.iv_pic);
+
+        iv_camera = (ImageView) findViewById(R.id.iv_camera);
+
+        iv_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkCameraPermission();
+            }
+        });
+
+    }
+
+    /**
+     * Method to launch camera after permission accepted from user
+     */
+    void takePicture() {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, 0);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+        if (resultCode == Activity.RESULT_OK && requestCode == 0) {
+
+            //this is your bitmap image and now you can do whatever you want with this
+
+            Bitmap picture = (Bitmap) data.getExtras().get("data");
+
+            iv_pic.setImageBitmap(picture);
+
+
+        }
+    }
+
+    /**
+     * Method to request permission for camera
+     */
+    private void requestCameraPermission() {
+
+
+        // Camera permission has not been granted yet. Request it directly.
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                REQUEST_CAMERA);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CAMERA) {
+            // BEGIN_INCLUDE(permission_result)
+            // Received permission result for camera permission.
+            Log.i(TAG, "Received response for Camera permission request.");
+
+            // Check if the only required permission has been granted
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                // Camera permission has been granted, preview can be displayed
+
+                takePicture();
+
+            } else {
+                //Permission not granted
+                Toast.makeText(MainActivity.this,"You need to grant camera permission to use camera",Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    /**
+     * Method to check permission
+     */
+    void checkCameraPermission() {
+        boolean isGranted;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Camera permission has not been granted.
+
+            requestCameraPermission();
+
+
+        } else {
+
+            takePicture();
+
+        }
+    }
+}
---
+++ app/src/main/java/com/app/marshmallowpermission/gpsPermissionCheckWithAndroidVersion/services/MyGpsService.java
@@ -0,0 +1,22 @@
+package com.app.marshmallowpermission.gpsPermissionCheckWithAndroidVersion.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Android on 08-Dec-16.
+ */
+
+public class MyGpsService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+
+
+}
