abdc58d84592208cbc99bba175b2e67fb087cab8
Android <mohitlivetech2016@gmail.com>
2016-12-09 16:40:32 +0530

    9 dec

--- app/src/main/java/com/app/marshmallowpermission/gpsPermissionCheckWithAndroidVersion/activity/MainGpsPermissionActivity.java
+++ app/src/main/java/com/app/marshmallowpermission/gpsPermissionCheckWithAndroidVersion/activity/MainGpsPermissionActivity.java
@@ -6,18 +6,46 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.app.marshmallowpermission.R;
 
 public class MainGpsPermissionActivity extends AppCompatActivity {
+
+    private Button mBtnStartService,mBtnStopService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_gps_permission);
 
+        mBtnStartService=(Button)findViewById(R.id.idBtnGpsPermissionCheck_StartService);
+        mBtnStopService=(Button)findViewById(R.id.idBtnGpsPermissionCheck_StopService);
 
-        
+        mBtnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+        mBtnStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+
+
+
+
+
+
+
     }
 
 }
